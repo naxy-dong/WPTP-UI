23c605e26c5e9da7467054f5476b67b3024043c6
YuXiang Dong <yuxiang.dong@stonybrook.edu>
2023-11-14 11:58:32 -0500

    Made the answer choices selectable

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -7,9 +7,11 @@ export default function Question({ number, content, prompt, answerChoices }) {
             <div>{prompt}</div>
             {
                 answerChoices.map(choice => (
-                    <div key={choice.letter}>
-                        <span>{choice.letter}: </span>
-                        <span>{choice.text}</span>
+                    <div key={choice.letter} className="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3">
+                        <label className="options">{choice.text}
+                            <input type="radio" name="radio" />
+                            <span className="checkmark">{choice.letter.toUpperCase()}</span>
+                        </label>  
                     </div>
                 ))
             }
